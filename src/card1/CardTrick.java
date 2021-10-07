e9b28a1990f10e41427081e6e16866d282198c3b
hasaali <hasaali@sheridancollage.ca>
2021-10-07 00:23:57 -0400

    This is first commit

---
+++ src/card1/CardTrick.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package card1;
+
+
+/**
+ *
+ * @author Ronak
+ */
+public class CardTrick {
+    public static void main(String[] args)
+    {
+       
+    }
+    
+}
